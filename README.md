7bc65cb6f4661d3169dabb8369d56e04054da9cd
Carlos Tovar <cartovarc@gmail.com>
2021-04-18 14:10:21 -0500

    first upload

---
+++ README.md
@@ -0,0 +1,32 @@
+## Install GO for NVIDIA Jetson NANO (https://golang.org/dl/)
+- Download Go for ARM64 linux, example go1.16.3.linux-arm64.tar.gz
+- sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.3.linux-arm64.tar.gz
+- echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+
+## Test this project
+
+### Download Pion examples
+cd $HOME/Desktop
+git clone https://github.com/pion/example-webrtc-applications.git
+export GO111MODULE=on
+go get github.com/pion/example-webrtc-applications/v3/gstreamer-send
+
+### Replace gstreamer-send Pion example by our own
+rm -rf $HOME/Desktop/example-webrtc-applications/gstreamer-send
+git clone https://github.com/cartovarc/gstreamer-send-playground.git
+mv $HOME/Desktop/gstreamer-send-playground $HOME/Desktop/example-webrtc-applications/gstreamer-send
+
+### Open with VS Code
+code $HOME/example-webrtc-applications/gstreamer-send
+
+### Run
+- Open html/demo.html on a browser
+- Copy SDP to activate.sh
+- source activate.sh
+- Copy terminal SDP in browser and "Start Session"
+
+
+## Utils
+
+### Discover devices on same net
+nmap -T5 -sP 192.168.10.*
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/pion/webrtc/v3"
+
+	gst "github.com/pion/example-webrtc-applications/v3/internal/gstreamer-src"
+	"github.com/pion/example-webrtc-applications/v3/internal/signal"
+)
+
+func main() {
+	audioSrc := flag.String("audio-src", "audiotestsrc", "GStreamer audio src")
+	videoSrc := flag.String("video-src", "videotestsrc", "GStreamer video src")
+	flag.Parse()
+
+	// Everything below is the pion-WebRTC API! Thanks for using it ❤️.
+
+	// Prepare the configuration
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+
+	// Create a audio track
+	audioTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "audio/opus"}, "audio", "pion1")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(audioTrack)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a video track
+	firstVideoTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "video/vp8"}, "video", "pion2")
+	if err != nil {
+		panic(err)
+	}
+	_, err = peerConnection.AddTrack(firstVideoTrack)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the offer to be pasted
+	offer := webrtc.SessionDescription{}
+	signal.Decode(signal.MustReadStdin(), &offer)
+
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(offer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create an answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output the answer in base64 so we can paste it in browser
+	fmt.Println(signal.Encode(*peerConnection.LocalDescription()))
+
+	// Start pushing buffers on these tracks
+	gst.CreatePipeline("opus", []*webrtc.TrackLocalStaticSample{audioTrack}, *audioSrc).Start()
+	gst.CreatePipeline("vp8", []*webrtc.TrackLocalStaticSample{firstVideoTrack}, *videoSrc).Start()
+
+	// Block forever
+	select {}
+}
